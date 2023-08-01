01fe5223c255c4e85910ad501a4fa65aa864f3c5
dbotti <davide.botti@lastminute.com>
2023-08-01 10:41:57 +0200

    Fix

--- FetchGithubUsers.js
+++ FetchGithubUsers.js
@@ -2,7 +2,5 @@ import fetch from 'node-fetch';
 
 export const fetchUsers = async () => {
     const response = await fetch('https://api.github.com/users/github');
-    const body = await response.json();
-    console.log(body);
-    return body;
+    return  await response.json();
 }
